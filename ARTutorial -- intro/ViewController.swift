9a0562acb5cbcb2d4d59cf89c69f254f67c9c2c7
Mahmud Rahman <mahmud@Mahmuds-MacBook-Pro.local>
2018-06-10 21:57:14 +0800

    complete

--- ARTutorial -- intro/ViewController.swift
+++ ARTutorial -- intro/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
-    override func viewDidLoad() {
+override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
@@ -24,10 +24,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +40,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        configuration.planeDetection = .horizontal
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -76,5 +79,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Your code goes here
+        if let anchor = anchor as? ARPlaneAnchor {
+            // Your code goes here
+            let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+            plane.materials.first?.diffuse.contents = UIColor.green.withAlphaComponent(0.5)
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.position = SCNVector3(CGFloat(anchor.center.x), CGFloat(anchor.center.y), CGFloat(anchor.center.z))
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+        }
     }
 }
